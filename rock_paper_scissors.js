9676674167b14a885e5b2ce046773d49ab75225c
Jeppe <jeppe-juul@hotmail.com>
2024-05-16 00:12:58 +0200

    [bugfix]fix computerChoice if statements

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,2 +1,15 @@
-let humanChoice = prompt("Please choose rock, paper or scissor")
+let humanChoice = prompt("Please choose rock, paper or scissor");
+let computerChoice = " "
 
+let randomNumber; 
+randomNumber = Math.random();
+if (randomNumber < 0.33) {
+    clearTimeoutomputerChoice = "rock"
+}
+else if (randomNumber < 0.66) {
+    computerChoice = "paper"
+}
+else 
+    computerChoice = "scissors";
+
+    
