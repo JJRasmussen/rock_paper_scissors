ec9d73da7bc9a0e7be19392f1e973312dc944741
Jeppe <jeppe-juul@hotmail.com>
2024-05-22 22:36:43 +0200

    [function]Add playRound function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,35 +1,55 @@
-let humanChoice = prompt("Please choose rock, paper or scissor");
-let computerChoice = " "
-
-let randomNumber; 
-randomNumber = Math.random();
-if (randomNumber < 0.33) {
-    clearTimeoutomputerChoice = "rock"
-}
-else if (randomNumber < 0.66) {
-    computerChoice = "paper"
-}
-else 
-    computerChoice = "scissors";
-
+let humanChoice = " ";
+let computerChoice = " ";
+let humanScore = 0;
+let computerScore = 0;
+let randomNumber = 0; 
 let winner = " "
 
-if (humanChoice === computerChoice) {
-    winner = "stalemate"
+function playRound(humanChoice, computerChoice) {
+    humanChoice = prompt("Please choose rock, paper or scissor");
+    humanChoice =  humanChoice.toLowerCase();
+    
+    computerChoice = " "
+
+        randomNumber = Math.random();
+    if (randomNumber < 0.33) {
+        computerChoice = "rock"
+    }
+    else if (randomNumber < 0.66) {
+        computerChoice = "paper"
+    }
+    else 
+        computerChoice = "scissors";
+
+    if (humanChoice === computerChoice) {
+        winner = "stalemate"
+        console.log("It's a stalemate, please try again")
+    }
+        else if (humanChoice === "rock") {
+            if (computerChoice === "paper"){
+                winner = "computer";
+                console.log("You lose, paper beats rock");
+            }
+            else winner = "human"
+                console.log("Congratulations, rock beats scissors")
+        }
+        else if (humanChoice === "paper") {
+            if (computerChoice === "scissors"){
+                winner = "computer";
+                console.log("You lose, scissors beats paper");
+            }
+            else winner = "human"
+                console.log("Congratulations, paper beats rock")
+        }
+        else if (computerChoice ==="rock"){
+            winner = "computer"
+            console.log("You lose, paper beats rock")
+        }
+            else(winner = "human")
+            console.log("Congratulations, rock beats scissors");
+    
+    if (winner = "human")
+    humanScore = humanScore + 1;
+    else
+    computerScore = computerScore + 1;
 }
-    else if (humanChoice === "rock") {
-        if (computerChoice === "paper"){
-            winner = "computer"
-        }
-        else winner = "human"
-    }
-    else if (humanChoice === "paper") {
-        if (computerChoice === "scissors"){
-            winner = "computer"
-        }
-        else winner = "human"
-    }
-    else if (computerChoice ==="rock"){
-        winner = "computer"
-    }
-        else(winner = "human")
